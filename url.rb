0d514f41b46e6166d79a63cb17e3c3fb3f9938ff
ShoIwase <i.shokun1021@gmail.com>
2017-01-25 21:03:46 +0900

    lo

--- url.rb
+++ url.rb
@@ -6,6 +6,7 @@ doc = Nokogiri.HTML(open(
   'https://twitter.com/search?f=tweets&vertical=default&q=%E3%81%82%E3%81%9F%E3%81%BE%E3%81%84%E3%81%9F%E3%81%84&src=typd'))
 
 doc.xpath("//li[@date-item-type='tweet']").each{ |tweet|
+  
  #Tweet時間
  puts Time.at(tweet.xpath(
   ".//a[@class='tweet-timestamp js-permalink js-nav js-tooltip']/span").first['date-time'].to_i)
